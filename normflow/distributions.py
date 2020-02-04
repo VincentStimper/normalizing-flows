70472767a8fd81d133ca1e6671c614b9e84d9fea
Vincent Stimper <vincent.stimper@gmail.com>
2020-02-04 14:58:36 +0000

    Gaussian added to cond distributions

--- normflow/distributions.py
+++ normflow/distributions.py
@@ -1,13 +1,15 @@
 import torch
 import torch.nn as nn
+import numpy as np
 
 class ParametrizedConditionalDistribution(nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(self, x):
+    def forward(self, x, num_sample=1):
         """
         :param x: Variable to condition on, first dimension is batch size
+        :param num_sample: number of samples to draw per element of mini-batch
         :return: sample of z for x, log probability for sample
         """
         raise NotImplementedError
@@ -17,12 +19,39 @@ class Dirac(ParametrizedConditionalDistribution):
     def __init__(self):
         super().__init__()
 
-    def forward(self, x):
-        z = x
-        if x.size() == torch.Size([]):
-            log_p = torch.zeros(x.size())
+    def forward(self, x, num_sample=1):
+        z = x.unsqueeze(1).repeat(1, num_sample, 1)
+        log_p = torch.zeros(z.size()[0:2])
+        return z, log_p
+
+
+class ConstDiagGaussian(ParametrizedConditionalDistribution):
+    def __init__(self, loc, scale):
+        """
+        Multivariate Gaussian distribution with diagonal covariance and parameters being constant wrt x
+        :param loc: mean vector of the distribution
+        :param scale: vector of the standard deviations on the diagonal of the covariance matrix
+        """
+        super().__init__()
+        self.n = loc.size()[0]
+        self.loc = nn.Parameter(loc.reshape((1, 1, self.n)))
+        self.scale = nn.Parameter(scale.reshape((1, 1, self.n)))
+        self.eps_dist = torch.distributions.normal.Normal(0, 1)
+
+
+    def forward(self, x=None, num_sample=1):
+        """
+        :param x: Variable to condition on, will only be used to determine the batch size
+        :param num_sample: number of samples to draw per element of mini-batch
+        :return: sample of z for x, log probability for sample
+        """
+        if x is not None:
+            batch_size = x.size()
         else:
-            log_p = torch.zeros(x.size()[0])
+            batch_size = 1
+        eps = self.eps_dist.sample((batch_size, num_sample, self.n))
+        z = self.loc + self.scale * eps
+        log_p = - 0.5 * self.n * np.log(2 * np.pi) - 0.5 * torch.sum(torch.log(self.scale) + ((z - self.loc) / self.scale) ** 2, 2)
         return z, log_p
 
 
