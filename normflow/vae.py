a6746b5bfb3e4847ac175cd466b1d3da10ab61e1
Lukas Ryll <43236963+lukasryll@users.noreply.github.com>
2020-02-24 12:27:39 +0000

    Update vae.py

--- normflow/vae.py
+++ normflow/vae.py
@@ -8,6 +8,8 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 import argparse
 import pandas as pd
+import os
+from datetime import datetime
 
 parser = argparse.ArgumentParser(description='Vanilla VAE implementation on MNIST')
 parser.add_argument('--batch-size', type=int, default=128, metavar='N',
@@ -24,11 +26,10 @@ parser.add_argument('--log-intv', type=int, default=20, metavar='N',
                     help='Training log status interval (default: 20')
 parser.add_argument('--experiment_mode', type=bool, default=False, metavar='N',
                     help='Experiment mode (conducts 10 runs and saves results as DataFrame (default: False)')
-parser.add_argument('--runs', type=int, default=False, metavar='N',
+parser.add_argument('--runs', type=int, default=10, metavar='N',
                     help='Number of runs in experiment_mode (experiment_mode has to be turned to True to use) (default: 10)')
 args = parser.parse_args()
 
-torch.manual_seed(args.seed)
 
 if args.dataset == 'mnist':
     img_dim = 28
@@ -37,19 +38,20 @@ elif args.dataset == 'cifar10' or args.dataset == 'cifar100':
 else:
     raise ValueError('The only dataset calls supported are: mnist, cifar10, cifar100')
 
+
 class VAE(nn.Module):
     def __init__(self):
         super().__init__()
-        self.encode = nn.Sequential(nn.Linear(img_dim**2, 400), nn.ReLU(True), nn.Linear(400, 200), nn.ReLU(True))
+        self.encode = nn.Sequential(nn.Linear(img_dim ** 2, 400), nn.ReLU(True), nn.Linear(400, 200), nn.ReLU(True))
         self.f1 = nn.Linear(200, 50)
         self.f2 = nn.Linear(200, 50)
         self.decode = nn.Sequential(nn.Linear(50, 200), nn.ReLU(True), nn.Linear(200, 400), nn.ReLU(True),
-                                    nn.Linear(400, img_dim**2))
+                                    nn.Linear(400, img_dim ** 2))
 
     def forward(self, x):
         # Encode
-        mu, log_var = self.f1(self.encode(x.view(x.size(0)*x.size(1), img_dim**2))), \
-                      self.f2(self.encode(x.view(x.size(0)*x.size(1), img_dim**2)))
+        mu, log_var = self.f1(self.encode(x.view(x.size(0) * x.size(1), img_dim ** 2))), \
+                      self.f2(self.encode(x.view(x.size(0) * x.size(1), img_dim ** 2)))
 
         # Reparametrize variables
         std = torch.exp(0.5 * log_var)
@@ -64,15 +66,9 @@ class VAE(nn.Module):
         return out, mu, log_var
 
 
-
 # Reconstruction + KL divergence losses summed over all elements and batch
 def loss_function(recon_x, x, mu, logvar):
     BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
-
-    # see Appendix B from VAE paper:
-    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
-    # https://arxiv.org/abs/1312.6114
-    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
     return BCE + KLD
@@ -113,14 +109,14 @@ train_loader, test_loader = flow_vae_datasets(args.dataset)
 
 
 # Train
-def train(epoch):
+def train(model, epoch):
     model.train()
     tr_loss = 0
     progressbar = tqdm(enumerate(train_loader), total=len(train_loader))
     for batch_n, (x, n) in progressbar:
         optimizer.zero_grad()
         rc_batch, mu, log_var = model(x)
-        loss = loss_function(rc_batch, x.view(x.size(0)*x.size(1), img_dim**2), mu, log_var)
+        loss = loss_function(rc_batch, x.view(x.size(0) * x.size(1), img_dim ** 2), mu, log_var)
         avg_loss = loss / len(x)
         avg_loss.backward()
         tr_loss += loss.item()
@@ -135,7 +131,8 @@ def train(epoch):
     print('====> Epoch: {} Average loss: {:.4f}'.format(
         epoch, tr_loss / len(train_loader.dataset)))
 
-def test(epoch):
+
+def test(model, epoch):
     model.eval()
     test_loss = 0
     with torch.no_grad():
@@ -147,12 +144,38 @@ def test(epoch):
     print('====> Test set loss: {:.4f}'.format(test_loss))
     return test_loss
 
+
 test_losses = []
 if __name__ == '__main__':
     if args.experiment_mode:
-        for i in args.runs:
+        min_test_losses = []
+        min_test_losses.append(str(args))
+        for i in range(args.runs):
+            model.__init__()
+            optimizer = optim.Adam(model.parameters(), lr=0.001)
+            if i == 0:
+                seed = args.seed
+            else:
+                seed+=1
+            torch.manual_seed(seed)
             for e in range(args.epochs):
-                train(e)
-                tl = test(e)
+                train(model, e)
+                tl = test(model, e)
                 test_losses.append(tl)
             print('====> Lowest test set loss: {:.4f}'.format(min(test_losses)))
+            min_test_losses.append(min(test_losses))
+        Series = pd.Series(min_test_losses)
+
+        dirName = 'experiments'
+        if not os.path.exists(dirName):
+            os.mkdir(dirName)
+        else:
+            pass
+        file_name = dirName + '/{}.xlsx'.format(str(datetime.now()))
+        file_name = file_name.replace(':', '-')
+        Series.to_excel(file_name, index=False, header=None)
+    else:
+        for e in range(args.epochs):
+            train(model,e)
+            tl = test(model, e)
+            test_losses.append(tl)
