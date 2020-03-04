8ad72c32a49e6f3ecd39af2d3b27fa36a42cf697
Vincent Stimper <vincent.stimper@gmail.com>
2020-03-04 17:42:28 +0000

    Add sigmoid before output

--- normflow/nets.py
+++ normflow/nets.py
@@ -5,10 +5,11 @@ class FCN(nn.Module):
     """
     Fully Connected neural Network with ReLUs as nonlinearities
     """
-    def __init__(self, hidden_units):
+    def __init__(self, hidden_units, sigmoid_output=False):
         """
         Constructor
         :param hidden_units: list of hidden units per layer
+        :param sigmoid_output: add sigmoid as final output layer
         """
         super().__init__()
 
@@ -18,6 +19,8 @@ class FCN(nn.Module):
             self.layers.append(nn.Linear(hidden_units[i], hidden_units[i + 1]))
             self.layers.append(nn.ReLU(True))
         self.layers.append(nn.Linear(hidden_units[self.n_hidden_layers - 2], hidden_units[self.n_hidden_layers - 1]))
+        if sigmoid_output:
+            self.layers.append(nn.Sigmoid())
 
         self.layers_seq = nn.Sequential(*self.layers)
 
