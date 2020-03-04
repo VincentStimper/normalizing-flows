df650323f4c508b5e4de4ed6aaef250373b6b776
Vincent Stimper <vincent.stimper@gmail.com>
2020-03-04 14:17:43 +0000

    Fix reshape

--- normflow/distributions.py
+++ normflow/distributions.py
@@ -172,8 +172,7 @@ class NNDiagGaussianDecoder(Decoder):
         return mean, std
 
     def log_p(self, x, z):
-        x_size = x.size()
-        mean_std = self.net(z.view(-1, *z.size()[2:])).view(*z.size()[:2], x.size(2) // 2, *x.size()[3:])
+        mean_std = self.net(z.view(-1, *z.size()[2:])).view(*z.size()[:2], x.size(2) * 2, *x.size()[3:])
         n_hidden = mean_std.size()[2] // 2
         mean = mean_std[:, :, :n_hidden, ...]
         std = mean_std[:, :, n_hidden:(2 * n_hidden), ...]
