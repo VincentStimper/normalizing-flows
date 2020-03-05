dbf086c026ca9003c4637af07d37e38538878b13
Lukas Ryll <43236963+lukasryll@users.noreply.github.com>
2020-03-05 13:50:38 +0000

    Update simple_flow_model.py

--- normflow/simple_flow_model.py
+++ normflow/simple_flow_model.py
@@ -1,14 +1,15 @@
 import torch.nn as nn
+import torch
 
 class SimpleFlowModel(nn.Module):
     def __init__(self, flows):
         super().__init__()
-        self.flows = flows
+        self.flows = nn.ModuleList(flows)
 
-    def forward(self, x):
-        log_det_ = 0.
+    def forward(self, z):
+        ld = 0.
         for flow in self.flows:
-            z, log_det = flow(x)
-            log_det_ += log_det
-            z_ = z
-        return z_, log_det_
+            z, ld_ = flow(z)
+            ld += ld_
+
+        return z, ld
