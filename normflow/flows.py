cca70afec78c72c477dbbae8239cfc9ddb4091f2
davindicode <Davindi09@gmail.com>
2020-03-06 15:03:47 +0000

    debugging flows

--- normflow/flows.py
+++ normflow/flows.py
@@ -132,6 +132,9 @@ class AffineConstFlow(Flow):
         t = self.t if self.t is not None else z.new_zeros(z.size())
         z_ = (z - t) * torch.exp(-s)
         log_det = torch.sum(-s, dim=2)
+        print(z_.shape)
+        print(log_det.shape)
+        print("acf")
         return z_, log_det
        
         
@@ -141,8 +144,8 @@ class ActNorm(AffineConstFlow):
     where on the very first batch we clever initialize the s,t so that the output
     is unit gaussian. As described in Glow paper.
     """
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args):
+        super().__init__(*args)
         self.data_dep_init_done = False
     
     def forward(self, z):
@@ -165,14 +168,20 @@ class AffineHalfFlow(Flow):
     """
     def __init__(self, shape, parity, net_class=nets.MLP, nh=4, scale=True, shift=True):
         super().__init__()
-        self.dim = shape[0]
+        self.d_cpu = torch.prod(torch.tensor(shape))
+        self.register_buffer('d', self.d_cpu)
         self.parity = parity
-        self.s_cond = lambda x: x.new_zeros(x.size(0), x.size(1), self.dim // 2)
-        self.t_cond = lambda x: x.new_zeros(x.size(0), x.size(1), self.dim // 2)
+        
         if scale:
-            self.s_cond = net_class([self.dim // 2, nh, nh, self.dim // 2])
+            self.add_module('s_cond', net_class([self.d_cpu.numpy() // 2, nh, nh, self.d_cpu.numpy() // 2]))
+            #self.s_cond.to('cuda')
+        else:
+            self.s_cond = lambda x: x.new_zeros(x.shape[0], x.shape[1], self.d_cpu // 2)
         if shift:
-            self.t_cond = net_class([self.dim // 2, nh, nh, self.dim // 2])
+            self.add_module('t_cond', net_class([self.d_cpu.numpy() // 2, nh, nh, self.d_cpu.numpy() // 2]))
+            #self.t_cond.to('cuda')
+        else:
+            self.t_cond = lambda x: x.new_zeros(x.shape[0], x.shape[1], self.d_cpu // 2)
         
     def forward(self, z):
         z0, z1 = z[:, :, ::2], z[:, :, 1::2]
@@ -180,14 +189,17 @@ class AffineHalfFlow(Flow):
         if self.parity:
             z0, z1 = z1, z0
         # reshape because nn.Linear takes the form (batch, dim) while we have (batch, samples, dim)
-        s = self.s_cond(z0.reshape(-1, self.dim // 2).float()).reshape(bs, -1, self.dim // 2)
-        t = self.t_cond(z0.reshape(-1, self.dim // 2).float()).reshape(bs, -1, self.dim // 2)
+        s = self.s_cond(z0.reshape(-1, self.d_cpu // 2)).reshape(bs, -1, self.d_cpu // 2)
+        t = self.t_cond(z0.reshape(-1, self.d_cpu // 2)).reshape(bs, -1, self.d_cpu // 2)
         z_0 = z0 # untouched
         z_1 = torch.exp(s) * z1 + t
         if self.parity:
             z_0, z_1 = z_1, z_0
         z_ = torch.cat([z_0, z_1], dim=2)
         log_det = torch.sum(s, dim=2)
+        print(z_.shape)
+        print(log_det.shape)
+        print("ahf")
         return z_, log_det
     
     def inverse(self, z):
@@ -196,8 +208,8 @@ class AffineHalfFlow(Flow):
         if self.parity:
             z0, z1 = z1, z0
         # reshape because nn.Linear takes the form (batch, dim) while we have (batch, samples, dim)
-        s = self.s_cond(z0.reshape(-1, self.dim // 2).float()).reshape(bs, -1, self.dim // 2)
-        t = self.t_cond(z0.reshape(-1, self.dim // 2).float()).reshape(bs, -1, self.dim // 2)
+        s = self.s_cond(z0.reshape(-1, self.d_cpu // 2)).reshape(bs, -1, self.d_cpu // 2)
+        t = self.t_cond(z0.reshape(-1, self.d_cpu // 2)).reshape(bs, -1, self.d_cpu // 2)
         x0 = z0 # this was the same
         x1 = (z1 - t) * torch.exp(-s) # reverse the transform on this half
         if self.parity:
@@ -210,8 +222,9 @@ class AffineHalfFlow(Flow):
 class Invertible1x1Conv(Flow):
     def __init__(self, shape):
         super().__init__()
-        self.dim = shape[0]
-        Q = torch.nn.init.orthogonal_(torch.randn(self.dim, self.dim))
+        self.d_cpu = torch.prod(torch.tensor(shape))
+        self.register_buffer('d', self.d_cpu)
+        Q = torch.nn.init.orthogonal_(torch.randn(self.d_cpu, self.d_cpu))
         P, L, U = torch.lu_unpack(*Q.lu())
         self.P = P # remains fixed during optimization
         self.L = nn.Parameter(L) # lower triangular portion
@@ -220,22 +233,25 @@ class Invertible1x1Conv(Flow):
 
     def _assemble_W(self):
         # assemble W from its components (P, L, U, S)
-        L = torch.tril(self.L, diagonal=-1) + torch.diag(torch.ones(self.dim))
+        L = torch.tril(self.L, diagonal=-1) + torch.diag(torch.ones(self.d_cpu))
         U = torch.triu(self.U, diagonal=1)
         W = self.P @ L @ (U + torch.diag(self.S))
         return W
 
     def forward(self, z):
-        W = self._assemble_W()
+        W = self._assemble_W().float().to(z.device)
         z_ = z @ W
         log_det = torch.sum(torch.log(torch.abs(self.S)))
         return z_, log_det
 
     def inverse(self, z):
-        W = self._assemble_W()
+        W = self._assemble_W().float().to(z.device)
         W_inv = torch.inverse(W)
         z_ = z @ W_inv
         log_det = -torch.sum(torch.log(torch.abs(self.S)))
+        print(z_.shape)
+        print(log_det.shape)
+        print("1x1")
         return z_, log_det
 
 
@@ -248,15 +264,15 @@ class Glow(Flow):
     Invertible1x1Conv(dim=2)
     AffineHalfFlow(dim=2, parity=i%2, nh=32)
     """
-    def __init__(self, shape):
+    def __init__(self, shape, parity):
         """
         :param shape: shape of the latent variable z
         """
         super().__init__()
-        self.flows = [ActNorm(shape), Invertible1x1Conv(shape), AffineHalfFlow(shape)]
+        self.flows = [ActNorm(shape), Invertible1x1Conv(shape), AffineHalfFlow(shape, parity)]
 
     def forward(self, z):
-        log_det_tot = 0
+        log_det_tot = torch.zeros(z.shape[0], z.shape[1], device=z.device)
         for flow in self.flows:
             z, log_det = flow(z)
             log_det_tot -= log_det
