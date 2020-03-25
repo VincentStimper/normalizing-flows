9e408ffbbed909e5988c21209d2ed648d482f981
davindicode <Davindi09@gmail.com>
2020-03-25 16:56:44 +0000

    cleaned up notebook

--- normflow/flows.py
+++ normflow/flows.py
@@ -69,23 +69,6 @@ class Planar(Flow):
         if log_det.dim() == 1:
             log_det = log_det.unsqueeze(1)
         return z_, log_det
-    
-    def inverse(self, z):
-        if self.h == torch.tanh:
-            inner = torch.sum(self.w * self.u)
-            u = self.u + (torch.log(1 + torch.exp(inner)) - 1 - inner) * self.w / torch.sum(self.w ** 2)
-            h_ = lambda x: 1 / torch.cosh(x) ** 2
-        else:
-            raise NotImplementedError('Nonlinearity is not implemented.')
-        lin = torch.sum(self.w * z, list(range(2, self.w.dim())), keepdim=True) + self.b
-        z_ = z + u * self.h(lin)
-        log_det = torch.log(torch.abs(1 + torch.sum(self.w * u) * h_(lin.squeeze())))
-        if log_det.dim() == 0:
-            log_det = log_det.unsqueeze(0)
-        if log_det.dim() == 1:
-            log_det = log_det.unsqueeze(1)
-        return z_, log_det
-
 
 class Radial(Flow):
     """
@@ -181,18 +164,16 @@ class Invertible1x1Conv(Flow):
     def __init__(self, shape):
         super().__init__()
         self.d_cpu = torch.prod(torch.tensor(shape))
-        self.register_buffer('d', self.d_cpu)
         Q = torch.nn.init.orthogonal_(torch.randn(self.d_cpu, self.d_cpu))
         P, L, U = torch.lu_unpack(*Q.lu())
-        self.register_buffer('P', P)
-        #self.P = P # remains fixed during optimization
+        self.register_buffer('P', P) # remains fixed during optimization
         self.L = nn.Parameter(L) # lower triangular portion
         self.S = nn.Parameter(U.diag()) # "crop out" the diagonal to its own parameter
         self.U = nn.Parameter(torch.triu(U, diagonal=1)) # "crop out" diagonal, stored in S
 
     def _assemble_W(self):
         # assemble W from its components (P, L, U, S)
-        L = torch.tril(self.L, diagonal=-1) + torch.diag(torch.ones(self.d, device=self.d.device))
+        L = torch.tril(self.L, diagonal=-1) + torch.diag(torch.ones(self.d_cpu, device=self.P.device))
         U = torch.triu(self.U, diagonal=1)
         W = self.P @ L @ (U + torch.diag(self.S))
         return W.float()
@@ -274,5 +255,12 @@ class Glow(Flow):
         log_det_tot = torch.zeros(z.shape[0], z.shape[1], device=z.device)
         for flow in self.flows:
             z, log_det = flow(z)
-            log_det_tot -= log_det
+            log_det_tot += log_det
+        return z, log_det_tot
+
+    def inverse(self, z):
+        log_det_tot = torch.zeros(z.shape[0], z.shape[1], device=z.device)
+        for flow in self.flows:
+            z, log_det = flow.inverse(z)
+            log_det_tot += log_det
         return z, log_det_tot
