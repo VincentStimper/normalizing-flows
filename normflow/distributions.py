31280e2cbc9560e8610d4b281e7543a066a8cd20
davindicode <Davindi09@gmail.com>
2020-03-25 12:29:14 +0000

    all underflow and overflow issues in target distributions solved

--- normflow/distributions.py
+++ normflow/distributions.py
@@ -258,10 +258,13 @@ class TwoModes(PriorDistribution):
             z_ = z.permute((z.dim() - 1, ) + tuple(range(0, z.dim() - 1)))
         else:
             z_ = z
+            
+        a = torch.abs(z_[0])
+        eps = torch.abs(torch.tensor(self.loc))
 
         log_prob = - 0.5 * ((torch.norm(z_, dim=0) - self.loc) / (2 * self.scale)) ** 2\
-                   - 0.5 * ((torch.abs(z_[0]) + torch.abs(torch.tensor(self.loc))) / (3 * self.scale)) ** 2\
-                   + torch.log(1 + torch.exp(2 * torch.abs(z_[0] * self.loc) / (3 * self.scale) ** 2))
+                   - 0.5 * ((a - eps) / (3 * self.scale)) ** 2\
+                   + torch.log(1 + torch.exp(-2 * (a * eps) / (3 * self.scale) ** 2))
         
         return log_prob
     
@@ -278,7 +281,7 @@ class Sinusoidal(PriorDistribution):
 
     def log_prob(self, z):
         """
-        log(p) = 1/2 * ((z[1] - w_1(z)) / (2 * scale)) ** 2
+        log(p) = - 1/2 * ((z[1] - w_1(z)) / (2 * scale)) ** 2
         w_1(z) = sin(2*pi / period * z[0])
         :param z: value or batch of latent variable
         :return: log probability of the distribution for z
@@ -289,7 +292,8 @@ class Sinusoidal(PriorDistribution):
             z_ = z
             
         w_1 = lambda x: torch.sin(2*np.pi / self.period * z_[0])
-        log_prob = - 0.5 * ((z_[1] - w_1(z_)) / (self.scale)) ** 2
+        log_prob = - 0.5 * ((z_[1] - w_1(z_)) / (self.scale)) ** 2 \
+                    #- 0.5 * (torch.norm(z_, dim=0, p=4) / (20 * self.scale)) ** 4
         
         return log_prob
     
@@ -319,8 +323,13 @@ class Sinusoidal_gap(PriorDistribution):
             
         w_1 = lambda x: torch.sin(2*np.pi / self.period * z_[0])
         w_2 = lambda x: self.w2_amp * torch.exp(-0.5*((z_[0] - self.w2_mu) / self.w2_scale)**2)
-        log_prob = -0.5 * ( (z_[1] - w_1(z_)) / self.scale) ** 2 + \
-                    torch.log(1 + torch.exp(-( (z_[1] - w_1(z_) + 0.5*w_2(z_)) * w_2(z_) / self.scale**2)))
+
+        eps = torch.abs(w_2(z_)/2)
+        a = torch.abs(z_[1] - w_1(z_) + w_2(z_)/2)
+        
+        log_prob = -0.5 * ((a - eps) / self.scale) ** 2 + \
+                    torch.log(1 + torch.exp(-2 * (eps * a) / self.scale**2)) \
+                    #- 0.5 * (torch.norm(z_, dim=0, p=4) / (20 * self.scale)) ** 4
         
         return log_prob
     
@@ -350,8 +359,13 @@ class Sinusoidal_split(PriorDistribution):
             
         w_1 = lambda x: torch.sin(2*np.pi / self.period * z_[0])
         w_3 = lambda x: self.w3_amp * torch.sigmoid((z_[0] - self.w3_mu) / self.w3_scale)
-        log_prob = -0.5 * ((z_[1] - w_1(z_)) / (self.scale)) ** 2 + \
-                    torch.log(1 + torch.exp(-( (z_[1] - w_1(z_) + 0.5*w_3(z_)) * w_3(z_) / self.scale**2)))
+        
+        eps = torch.abs(w_3(z_)/2)
+        a = torch.abs(z_[1] - w_1(z_) + w_3(z_)/2)
+        
+        log_prob = -0.5 * ((a - eps) / (self.scale))**2 + \
+                    torch.log(1 + torch.exp(-2 * (eps * a) / self.scale**2)) \
+                    #- 0.5 * (torch.norm(z_, dim=0, p=4) / (20 * self.scale)) ** 4
         
         return log_prob
     
