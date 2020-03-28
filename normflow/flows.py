Corrected masking

normflow/flows.py
@@ -160,22 +160,19 @@ class MaskedAffineFlow(Flow):
             self.t = lambda x: x.new_zeros(*x.size())
 
     def forward(self, z):
-        z_size = z.size()
         z_masked = self.b * z
-        scale = self.s(z)
-        trans = self.t(z)
+        scale = self.s(z_masked)
+        trans = self.t(z_masked)
         z_ = z_masked + (1 - self.b) * (z * torch.exp(scale) + trans)
         log_det = torch.sum((1 - self.b) * scale, dim=list(range(1, self.b.dim())))
         return z_, log_det
 
     def inverse(self, z):
-        z_size = z.size()
         z_masked = self.b * z
-        z_bd_flatten = z_masked.view(-1, *z_size[2:])
-        scale = self.s(z_bd_flatten).view(*z_size)
-        trans = self.t(z_bd_flatten).view(*z_size)
+        scale = self.s(z_masked)
+        trans = self.t(z_masked)
         z_ = z_masked + (1 - self.b) * (z - trans) * torch.exp(-scale)
-        log_det = -torch.sum((1 - self.b) * scale, dim=list(range(2, self.b.dim())))
+        log_det = -torch.sum((1 - self.b) * scale, dim=list(range(1, self.b.dim())))
         return z_, log_det
 
 