ea13a7ad7a9e71fa3e21c3e622813972715b1a39
Vincent Stimper <vincent.stimper@gmail.com>
2020-04-02 13:40:33 +0100

    Diagonal Gaussian Base distribution added

--- normflow/distributions.py
+++ normflow/distributions.py
@@ -2,7 +2,62 @@ import torch
 import torch.nn as nn
 import numpy as np
 
+class BaseDistribution(nn.Module):
+    """
+    Base distribution of a flow-based model
+    Parameters do not depend of target variable (as is the case for a VAE encoder)
+    """
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, num_samples=1):
+        """
+        Samples from base distribution and calculates log probability
+        :param num_samples: Number of samples to draw from the distriubtion
+        :return: Samples drawn from the distribution, log probability
+        """
+        raise NotImplementedError
+
+    def log_prob(self, z):
+        """
+        Calculate log probability of batch of samples
+        :param z: Batch of random variables to determine log probability for
+        :return: log probability for each batch element
+        """
+        raise NotImplementedError
+
+
+class DiagGaussian(BaseDistribution):
+    """
+    Multivariate Gaussian distribution with diagonal covariance matrix
+    """
+    def __init__(self, d):
+        """
+        Constructor
+        :param d: Dimension of Gaussian distribution
+        """
+        super().__init__()
+        self.d = d
+        self.loc = nn.Parameter(torch.zeros(1, self.d))
+        self.log_scale = nn.Parameter(torch.zeros(1, self.d))
+
+    def forward(self, num_samples=1):
+        eps = torch.randn((num_samples, self.d), device=self.loc.device)
+        z = self.loc + torch.exp(self.log_scale) * eps
+        log_p = - 0.5 * self.d * np.log(2 * np.pi) - torch.sum(self.log_scale + 0.5 * torch.pow(eps, 2), 1)
+        return z, log_p
+
+    def log_prob(self, z):
+        log_p = - 0.5 * self.d * np.log(2 * np.pi)\
+                - torch.sum(self.log_scale + 0.5 * torch.pow((z - self.loc) / torch.exp(self.log_scale), 2), 1)
+        return log_p
+
+
 class BaseEncoder(nn.Module):
+    """
+    Base distribution of a flow-based variational autoencoder
+    Parameters of the distribution depend of the target variable x
+    """
     def __init__(self):
         super().__init__()
 
@@ -62,12 +117,12 @@ class ConstDiagGaussian(BaseEncoder):
         :param scale: vector of the standard deviations on the diagonal of the covariance matrix
         """
         super().__init__()
-        self.n = len(loc)
+        self.d = len(loc)
         if not torch.is_tensor(loc):
             loc = torch.tensor(loc).float()
         if not torch.is_tensor(scale):
             scale = torch.tensor(scale).float()
-        self.loc = nn.Parameter(loc.reshape((1, 1, self.n)))
+        self.loc = nn.Parameter(loc.reshape((1, 1, self.d)))
         self.scale = nn.Parameter(scale)
 
     def forward(self, x=None, num_samples=1):
@@ -80,9 +135,9 @@ class ConstDiagGaussian(BaseEncoder):
             batch_size = len(x)
         else:
             batch_size = 1
-        eps = torch.randn((batch_size, num_samples, self.n), device=x.device)
+        eps = torch.randn((batch_size, num_samples, self.d), device=x.device)
         z = self.loc + self.scale * eps
-        log_p = - 0.5 * self.n * np.log(2 * np.pi) - torch.sum(torch.log(self.scale) + 0.5 * torch.pow(eps, 2), 2)
+        log_p = - 0.5 * self.d * np.log(2 * np.pi) - torch.sum(torch.log(self.scale) + 0.5 * torch.pow(eps, 2), 2)
         return z, log_p
 
     def log_prob(self, z, x):
@@ -95,7 +150,7 @@ class ConstDiagGaussian(BaseEncoder):
             z = z.unsqueeze(0)
         if z.dim() == 2:
             z = z.unsqueeze(0)
-        log_p = - 0.5 * self.n * np.log(2 * np.pi) - torch.sum(torch.log(self.scale) + 0.5 * ((z - self.loc) / self.scale) ** 2, 2)
+        log_p = - 0.5 * self.d * np.log(2 * np.pi) - torch.sum(torch.log(self.scale) + 0.5 * ((z - self.loc) / self.scale) ** 2, 2)
         return log_p
 
 
