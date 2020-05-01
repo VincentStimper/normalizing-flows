70884a434bbab3df5499f272e24cc613717d2b54
Vincent Stimper <vincent.stimper@gmail.com>
2020-05-01 11:30:46 +0100

    Added device

--- normflow/flows.py
+++ normflow/flows.py
@@ -160,11 +160,10 @@ class MaskedAffineFlow(Flow):
     def forward(self, z):
         z_masked = self.b * z
         scale = self.s(z_masked)
-        scale = torch.where(torch.isfinite(scale), scale,
-                            torch.tensor(np.nan, dtype=scale.dtype))
+        nan = torch.tensor(np.nan, dtype=z.dtype, device=z.device)
+        scale = torch.where(torch.isfinite(scale), scale, nan)
         trans = self.t(z_masked)
-        trans = torch.where(torch.isfinite(trans), trans,
-                            torch.tensor(np.nan, dtype=trans.dtype))
+        trans = torch.where(torch.isfinite(trans), trans, nan)
         z_ = z_masked + (1 - self.b) * (z * torch.exp(scale) + trans)
         log_det = torch.sum((1 - self.b) * scale, dim=list(range(1, self.b.dim())))
         return z_, log_det
@@ -172,11 +171,10 @@ class MaskedAffineFlow(Flow):
     def inverse(self, z):
         z_masked = self.b * z
         scale = self.s(z_masked)
-        scale = torch.where(torch.isfinite(scale), scale,
-                            torch.tensor(np.nan, dtype=scale.dtype))
+        nan = torch.tensor(np.nan, dtype=z.dtype, device=z.device)
+        scale = torch.where(torch.isfinite(scale), scale, nan)
         trans = self.t(z_masked)
-        trans = torch.where(torch.isfinite(trans), trans,
-                            torch.tensor(np.nan, dtype=trans.dtype))
+        trans = torch.where(torch.isfinite(trans), trans, nan)
         z_ = z_masked + (1 - self.b) * (z - trans) * torch.exp(-scale)
         log_det = -torch.sum((1 - self.b) * scale, dim=list(range(1, self.b.dim())))
         return z_, log_det
