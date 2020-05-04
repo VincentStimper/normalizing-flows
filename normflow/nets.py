641315a0bea826c198f16e1c6736efda03641dcd
Vincent Stimper <vincent.stimper@gmail.com>
2020-05-04 13:33:36 +0100

    Added output function and scale to MLP

--- normflow/nets.py
+++ normflow/nets.py
@@ -1,17 +1,21 @@
 import torch
 from torch import nn
+from . import utils
 
 class MLP(nn.Module):
     """
     A multilayer perceptron with Leaky ReLU nonlinearities
     """
 
-    def __init__(self, layers, leaky=0.0, sigmoid_output=False, init_zeros=False):
+    def __init__(self, layers, leaky=0.0, output_fn=None, output_scale=1., init_zeros=False):
         """
         :param layers: list of layer sizes from start to end
         :param leaky: slope of the leaky part of the ReLU,
         if 0.0, standard ReLU is used
-        :param sigmoid_output: Flag, if true, sigmoid is applied to output
+        :param output_fn: String, function to be applied to the output, either
+        None, "sigmoid", "relu", or "tanh"
+        :param output_scale: Rescale outputs if output_fn is specified, i.e.
+        scale * output_fn(out / scale)
         :param init_zeros: Flag, if true, weights and biases of last layer
         are initialized with zeros (helpful for deep models, see arXiv 1807.03039)
         """
@@ -24,8 +28,17 @@ class MLP(nn.Module):
         if init_zeros:
             nn.init.zeros_(net[-1].weight)
             nn.init.zeros_(net[-1].bias)
-        if sigmoid_output:
-            net.append(nn.Sigmoid())
+        if output_fn is not None:
+            net.append(utils.ConstScaleLayer(1 / output_scale))
+            if output_fn is "sigmoid":
+                net.append(nn.Sigmoid())
+            elif output_fn is "relu":
+                net.append(nn.ReLU())
+            elif output_fn is "tanh":
+                net.append(nn.Tanh())
+            else:
+                NotImplementedError("This output function is not implemented.")
+            net.append(utils.ConstScaleLayer(output_scale))
         self.net = nn.Sequential(*net)
 
     def forward(self, x):
--- normflow/utils.py
+++ normflow/utils.py
@@ -1,3 +1,7 @@
+import torch
+from torch import nn
+
+
 def set_requires_grad(module, flag):
     """
     Sets requires_grad flag of all parameters of a torch.nn.module
@@ -7,3 +11,20 @@ def set_requires_grad(module, flag):
 
     for param in module.parameters():
         param.requires_grad = flag
+
+
+class ConstScaleLayer(nn.Module):
+    """
+    Scaling features by a fixed factor
+    """
+    def __init__(self, scale=1.):
+        """
+        Constructor
+        :param scale: Scale to apply to features
+        """
+        super().__init__()
+        self.scale_cpu = torch.tensor(scale)
+        self.register_buffer("scale", self.scale_cpu)
+
+    def forward(self, input):
+        return input * self.scale
