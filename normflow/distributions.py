4cb111a145ca6b89d89bd5c5ef3c309c89d01ee1
Vincent Stimper <vincent.stimper@gmail.com>
2020-05-07 15:57:41 +0100

    Distribution linearly interpolating two arbitrary dists added

--- normflow/distributions.py
+++ normflow/distributions.py
@@ -576,3 +576,24 @@ class DiagGaussianProposal(MHProposal):
         z_ = eps * self.scale + z
         log_p_diff = torch.zeros(num_samples, dtype=z.dtype, device=z.device)
         return z_, log_p_diff
+
+
+class LinearInterpolation:
+    """
+    Linear interpolation of two distributions in the log space
+    """
+    def __init__(self, dist1, dist2, alpha):
+        """
+        Constructor
+        :param dist1: First distribution
+        :param dist2: Second distribution
+        :param alpha: Interpolation parameter,
+        log_p = alpha * log_p_1 + (1 - alpha) * log_p_2
+        """
+        self.alpha = alpha
+        self.dist1 = dist1
+        self.dist2 = dist2
+
+    def log_prob(self, z):
+        return self.alpha * self.dist1.log_prob(z)\
+               + (1 - self.alpha) * self.dist2.log_prob(z)
