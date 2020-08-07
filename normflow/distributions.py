8b6ddd14cbc541b1e06c877671fc4285c8a16204
Vincent Stimper <vincent.stimper@gmail.com>
2020-08-07 19:47:33 +0100

    Resampled distribution added

--- normflow/distributions.py
+++ normflow/distributions.py
@@ -57,6 +57,76 @@ class DiagGaussian(BaseDistribution):
         return log_p
 
 
+class ResampledGaussian(BaseDistribution):
+    """
+    Multivariate Gaussian distribution with diagonal covariance matrix,
+    resampled according to a acceptance probability determined by a neural network,
+    see arXiv 1810.11428
+    """
+    def __init__(self, d, a, T, eps, trainable=True):
+        """
+        Constructor
+        :param d: Dimension of Gaussian distribution
+        :param a: Function returning the acceptance probability
+        :param T: Maximum number of rejections
+        :param eps: Discount factor in exponential average of Z
+        """
+        super().__init__()
+        self.d = d
+        self.a = a
+        self.T = T
+        self.eps = eps
+        self.Z = None
+        if trainable:
+            self.loc = nn.Parameter(torch.zeros(1, self.d))
+            self.log_scale = nn.Parameter(torch.zeros(1, self.d))
+        else:
+            self.register_buffer("loc", torch.zeros(1, self.d))
+            self.register_buffer("log_scale", torch.zeros(1, self.d))
+
+    def forward(self, num_samples=1):
+        t = 0
+        z = torch.zeros(num_samples, self.d, dtype=self.loc.dtype, device=self.loc.device)
+        s = 0
+        for i in range(self.T):
+            eps = torch.randn((num_samples, self.d), dtype=self.loc.dtype, device=self.loc.device)
+            z_ = self.loc + torch.exp(self.log_scale) * eps
+            acc = self.a(z)
+            dec = torch.randn_like(acc) < acc
+            for i, dec_ in enumerate(dec):
+                if dec_ or t == self.T:
+                    z[s] = z_[i]
+                    s = s + 1
+                    t = 0
+                else:
+                    t = t + 1
+                if s == self.d:
+                    break
+            if s == self.d:
+                break
+        log_p_gauss = - 0.5 * self.d * np.log(2 * np.pi) \
+                      - torch.sum(self.log_scale + 0.5 * torch.pow((z - self.loc) / torch.exp(self.log_scale), 2), 1)
+        acc = self.a(z)
+        alpha = (1 - self.Z) ** (self.T - 1)
+        log_p = torch.log((1 - alpha) * acc / self.Z + alpha) + log_p_gauss
+        return z, log_p
+
+    def log_prob(self, z):
+        log_p_gauss = - 0.5 * self.d * np.log(2 * np.pi) \
+                      - torch.sum(self.log_scale + 0.5 * torch.pow((z - self.loc) / torch.exp(self.log_scale), 2), 1)
+        acc = self.a(z)
+        Z_batch = torch.mean(acc)
+        if self.Z == None:
+            self.Z = Z_batch
+            Z_train = Z_batch
+        else:
+            self.Z = ((1 - self.eps) * self.Z + self.eps * Z_batch).detach()
+            Z_train = Z_batch - Z_batch.detach() + self.Z
+        alpha = (1 - Z_train) ** (self.T - 1)
+        log_p = torch.log((1 - alpha) * acc / Z_train + alpha) + log_p_gauss
+        return log_p
+
+
 class GaussianMixture(BaseDistribution):
     """
     Mixture of Gaussians with diagonal covariance matrix
