343f98a0d3b73a8cf0eabf0ab3d2f7a3bf05960f
Vincent Stimper <vincent.stimper@gmail.com>
2020-08-21 15:10:36 +0100

    Function for jittering added

--- normflow/utils.py
+++ normflow/utils.py
@@ -30,14 +30,37 @@ class ConstScaleLayer(nn.Module):
         return input * self.scale
 
 
-class LogitTransform():
+class Logit():
     """
     Transform for dataloader
     logit(alpha + (1 - alpha) * x) where logit(x) = log(x / (1 - x))
     """
-    def __init__(self, alpha):
+    def __init__(self, alpha=0):
+        """
+        Constructor
+        :param alpha: see above
+        """
         self.alpha = alpha
 
     def __call__(self, x):
         x_ = self.alpha + (1 - self.alpha) * x
         return torch.log(x_ / (1 - x_))
+
+
+class Jitter():
+    """
+    Transform for dataloader
+    Adds uniform jitter noise to data making sure that data stays in interval [0, 1
+    """
+    def __init__(self, scale=1./255):
+        """
+        Constructor
+        :param scale: Scaling factor for noise
+        """
+        self.scale
+
+    def __call__(self, x):
+        eps = (torch.rand_like(x) - 0.5) * self.scale
+        x_ = torch.abs(x + eps)
+        return x_ - 2 * torch.relu(1 - x_)
+
