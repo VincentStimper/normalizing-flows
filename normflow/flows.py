582356be85acf71b6e41053b68e4c471b336a632
Vincent Stimper <vincent.stimper@gmail.com>
2020-08-23 17:37:37 +0100

    Moved checkerboard pattern to device

--- normflow/flows.py
+++ normflow/flows.py
@@ -172,6 +172,7 @@ class Split(Flow):
                 cb1 = [cb1_ if j % 2 == 0 else cb0_ for j in range(z.size(n_dims - i))]
             cb = cb1 if 'inv' in self.mode else cb0
             cb = torch.tensor(cb)[None].repeat(len(z), *((n_dims - 1) * [1]))
+            cb = cb.to(z.device)
             z_size = z.size()
             z1 = z.reshape(-1)[cb.view(-1).nonzero()].view(*z_size[:-1], -1)
             z2 = z.reshape(-1)[(1 - cb).view(-1).nonzero()].view(*z_size[:-1], -1)
@@ -199,6 +200,7 @@ class Split(Flow):
                 cb1 = [cb1_ if j % 2 == 0 else cb0_ for j in range(z_size[n_dims - i])]
             cb = cb1 if 'inv' in self.mode else cb0
             cb = torch.tensor(cb)[None].repeat(z_size[0], *((n_dims - 1) * [1]))
+            cb = cb.to(z1.device)
             z1 = z1[..., None].repeat(*(n_dims * [1]), 2).view(*z_size[:-1], -1)
             z2 = z2[..., None].repeat(*(n_dims * [1]), 2).view(*z_size[:-1], -1)
             z = cb * z1 + (1 - cb) * z2
