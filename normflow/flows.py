1e78741f45babbf729b1eaa950025e63f237481d
Andrew Campbell <blank>
2020-08-27 15:59:47 +0100

    Added option to get acceptance probabilities

--- normflow/flows.py
+++ normflow/flows.py
@@ -673,7 +673,7 @@ class HamiltonianMonteCarlo(Flow):
         self.register_parameter('log_step_size', torch.nn.Parameter(log_step_size))
         self.register_parameter('log_mass', torch.nn.Parameter(log_mass))
 
-    def forward(self, z):
+    def forward(self, z, return_probabilities=False):
         # Draw momentum
         p = torch.randn_like(z) * torch.exp(0.5 * self.log_mass)
 
@@ -691,11 +691,20 @@ class HamiltonianMonteCarlo(Flow):
             self.target.log_prob(z_new) - self.target.log_prob(z) - \
             0.5 * torch.sum(p_new ** 2 / torch.exp(self.log_mass), 1) + \
             0.5 * torch.sum(p ** 2 / torch.exp(self.log_mass), 1))
+
+        probabilities = torch.where(probabilities > 1.0,
+            torch.ones_like(probabilities), probabilities)
+
         uniforms = torch.rand_like(probabilities)
         mask = uniforms < probabilities
         z_out = torch.where(mask.unsqueeze(1), z_new, z)
 
-        return z_out, self.target.log_prob(z) - self.target.log_prob(z_out)
+        if return_probabilities:
+            return z_out, \
+                self.target.log_prob(z) - self.target.log_prob(z_out), \
+                probabilities
+        else:
+            return z_out, self.target.log_prob(z) - self.target.log_prob(z_out)
 
     def inverse(self, z):
         return self.forward(z)
