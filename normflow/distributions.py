ea1e7319973d7ffdf1ecb0bdd53152398b02da8b
Vincent Stimper <vincent.stimper@gmail.com>
2020-09-22 12:00:51 +0100

    New class for target distributions

--- normflow/distributions.py
+++ normflow/distributions.py
@@ -809,6 +809,86 @@ class Smiley(PriorDistribution):
 
 
 
+# Target distributions
+
+class Target(nn.Module):
+    """
+    Sample target distributions to test models
+    """
+    def __init__(self, prop_scale=torch.tensor(1.),
+                 prop_shift=torch.tensor(0.)):
+        """
+        Constructor
+        :param prop_scale: Scale for the uniform proposal
+        :param prop_shift: Shift for the uniform proposal
+        """
+        super().__init__()
+        self.register_buffer("prop_scale", prop_scale)
+        self.register_buffer("prop_shift", prop_shift)
+
+    def log_prob(self, z):
+        """
+        :param z: value or batch of latent variable
+        :return: log probability of the distribution for z
+        """
+        raise NotImplementedError('The log probability is not implemented yet.')
+
+    def rejection_sampling(self, num_steps=1):
+        """
+        Perform rejection sampling on image distribution
+        :param num_steps: Number of rejection sampling steps to perform
+        :return: Accepted samples
+        """
+        eps = torch.rand((num_steps, self.n_dims), dtype=self.prop_scale.dtype,
+                         device=self.prop_scale.device)
+        z_ = self.prop_scale * eps + self.prop_shift
+        prob = torch.rand(num_steps, dtype=self.prop_scale.dtype,
+                          device=self.prop_scale.device)
+        prob_ = torch.exp(self.log_prob(z_) - self.max_log_prob)
+        accept = prob_ > prob
+        z = z_[accept, :]
+        return z
+
+    def sample(self, num_samples=1):
+        """
+        Sample from image distribution through rejection sampling
+        :param num_samples: Number of samples to draw
+        :return: Samples
+        """
+        z = torch.zeros((0, self.n_dims), dtype=self.prop_scale.dtype,
+                        device=self.prop_scale.device)
+        while len(z) < num_samples:
+            z_ = self.rejection_sampling(num_samples)
+            ind = np.min([len(z_), num_samples - len(z)])
+            z = torch.cat([z, z_[:ind, :]], 0)
+        return z
+
+
+class TwoMoons(Target):
+    """
+    Bimodal two-dimensional distribution
+    """
+    def __init__(self):
+        super().__init__(torch.tensor(6.), torch.tensor(3.))
+        self.n_dims = 2
+        self.max_log_prob = np.log(2.)
+
+    def log_prob(self, z):
+        """
+        log(p) = - 1/2 * ((norm(z) - 2) / 0.2) ** 2
+                 + log(  exp(-1/2 * ((z[0] - 2) / 0.3) ** 2)
+                       + exp(-1/2 * ((z[0] + 2) / 0.3) ** 2))
+        :param z: value or batch of latent variable
+        :return: log probability of the distribution for z
+        """
+        a = torch.abs(z[:, 0])
+        log_prob = - 0.5 * ((torch.norm(z, dim=1) - 2) / 0.2) ** 2 \
+                   - 0.5 * ((a - 2) / 0.3) ** 2 \
+                   + torch.log(1 + torch.exp(-4 * a / 0.09))
+        return log_prob
+
+
+
 class MHProposal(nn.Module):
     """
     Proposal distribution for the Metropolis Hastings algorithm
