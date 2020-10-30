442adacabbdc63a26ac8c66fb07e27d0990eb865
Vincent Stimper <vincent.stimper@gmail.com>
2020-10-30 12:57:07 +0000

    Logit transform fixed

--- normflow/transforms.py
+++ normflow/transforms.py
@@ -9,22 +9,16 @@ class Logit(flows.Flow):
     Logit mapping of image tensor, see RealNVP paper
     logit(alpha + (1 - alpha) * x) where logit(x) = log(x / (1 - x))
     """
-    def __init__(self, alpha=0.05, jitter=False, jitter_scale=1./255):
+    def __init__(self, alpha=0.05):
         """
         Constructor
         :param alpha: Alpha parameter, see above
         """
         super().__init__()
         self.alpha = alpha
-        self.jitter = jitter
-        self.jitter_scale = jitter_scale
 
     def forward(self, z):
-        if self.jitter:
-            beta = (1 - 2 * self.alpha) / (1 + self.jitter_scale)
-        else:
-            beta = 1 - 2 * self.alpha
-        #beta = 1 - self.alpha
+        beta = 1 - 2 * self.alpha
         sum_dims = list(range(1, z.dim()))
         ls = torch.sum(torch.nn.functional.logsigmoid(z), dim=sum_dims)
         mls = torch.sum(torch.nn.functional.logsigmoid(-z), dim=sum_dims)
@@ -33,16 +27,8 @@ class Logit(flows.Flow):
         return z, log_det
 
     def inverse(self, z):
-        # Apply scale jittering if needed
-        if self.jitter:
-            eps = torch.rand_like(z) * self.jitter_scale
-            beta = (1 - 2 * self.alpha) / (1 + self.jitter_scale)
-            z = self.alpha + beta * (z + eps)
-        else:
-            beta = 1 - 2 * self.alpha
-            z = self.alpha + beta * z
-        #beta = 1 - self.alpha
-        #z = self.alpha + beta * z
+        beta = 1 - 2 * self.alpha
+        z = self.alpha + beta * z
         logz = torch.log(z)
         log1mz = torch.log(1 - z)
         z = logz - log1mz
