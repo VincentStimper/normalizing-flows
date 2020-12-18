c58b183d8c7f05ffcafeb13f2d01dc36ba29aa4d
Vincent Stimper <vincent.stimper@gmail.com>
2020-12-18 16:08:44 +0100

    Print sys path for debugging

--- normflow/flows.py
+++ normflow/flows.py
@@ -8,6 +8,7 @@ from . import nets
 import sys
 from pathlib import Path
 sys.path.insert(0, str(Path(__file__).parents[0] / "submodules"))
+print(sys.path)
 try:
     from residual_flows.lib.layers import iResBlock
 finally:
