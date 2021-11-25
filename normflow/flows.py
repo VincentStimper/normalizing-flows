5f256391637cdca13cf0e243b04c31eb2425880e
Vincent Stimper <vincent.stimper@gmail.com>
2021-11-25 11:21:43 +0100

    Neural spline flow layers added

--- normflow/flows.py
+++ normflow/flows.py
@@ -4,11 +4,21 @@ import torch.nn as nn
 
 from . import nets
 
-# Try importing Resflow dependencies
+# Try importing Residual Flow dependencies
 try:
     from residual_flows.layers import iResBlock
 except:
     print('Warning: Dependencies for Residual Flows could '
+          'not be loaded. Other models can still be used.')
+
+# Try importing Neural Spline Flow dependencies
+try:
+    from neural_spline_flows.nde.transforms.coupling import PiecewiseRationalQuadraticCouplingTransform
+    from neural_spline_flows.nde.transforms.autoregressive import MaskedPiecewiseRationalQuadraticAutoregressiveTransform
+    from neural_spline_flows.nn import ResidualNet
+    from neural_spline_flows.utils import create_alternating_binary_mask
+except:
+    print('Warning: Dependencies for Neural Spline Flows could '
           'not be loaded. Other models can still be used.')
 
 
@@ -549,6 +559,110 @@ class Residual(Flow):
         else:
             z, log_det = self.iresblock.inverse(z, 0)
         return z, -log_det.view(-1)
+
+
+# Neural Spline Flow layers
+
+class CoupledRationalQuadraticSpline(Flow):
+    def __init__(
+            self,
+            num_input_channels,
+            num_hidden_layers,
+            num_hidden_channels,
+            num_bins=8,
+            tail_bound=3,
+            activation=nn.ReLU,
+            dropout_probability=0.,
+            reverse_mask=False,
+            reverse=True
+    ):
+        super().__init__()
+        self.reverse = reverse
+
+        def transform_net_create_fn(in_features, out_features):
+            return ResidualNet(
+                in_features=in_features,
+                out_features=out_features,
+                context_features=None,
+                hidden_features=num_hidden_channels,
+                num_blocks=num_hidden_layers,
+                activation=activation(),
+                dropout_probability=dropout_probability,
+                use_batch_norm=False
+            )
+
+        self.prqct=PiecewiseRationalQuadraticCouplingTransform(
+            mask=create_alternating_binary_mask(
+                num_input_channels,
+                even=reverse_mask
+            ),
+            transform_net_create_fn=transform_net_create_fn,
+            num_bins=num_bins,
+            tails='linear',
+            tail_bound=tail_bound,
+
+            # Setting True corresponds to equations (4), (5), (6) in the NSF paper:
+            apply_unconditional_transform=True
+        )
+
+    def forward(self, z):
+        if self.reverse:
+            z, log_det = self.prqct.inverse(z)
+        else:
+            z, log_det = self.prqct(z)
+        return z, log_det.view(-1)
+
+    def inverse(self, z):
+        if self.reverse:
+            z, log_det = self.prqct(z)
+        else:
+            z, log_det = self.prqct.inverse(z)
+        return z, log_det.view(-1)
+
+
+class AutoregressiveRationalQuadraticSplineBijection(Flow):
+    def __init__(
+            self,
+            num_input_channels,
+            num_hidden_layers,
+            num_hidden_channels,
+            num_bins=8,
+            tail_bound=3,
+            activation=nn.ReLU,
+            dropout_probability=0.,
+            reverse=True
+    ):
+        super().__init__()
+        self.reverse = reverse
+
+        self.mprqat=MaskedPiecewiseRationalQuadraticAutoregressiveTransform(
+            features=num_input_channels,
+            hidden_features=num_hidden_channels,
+            context_features=None,
+            num_bins=num_bins,
+            tails='linear',
+            tail_bound=tail_bound,
+            num_blocks=num_hidden_layers,
+            use_residual_blocks=True,
+            random_mask=False,
+            activation=activation(),
+            dropout_probability=dropout_probability,
+            use_batch_norm=False)
+
+    def forward(self, z):
+        if self.reverse:
+            z, log_det = self.mprqat.inverse(z)
+        else:
+            z, log_det = self.mprqat(z)
+        return z, log_det.view(-1)
+
+    def inverse(self, z):
+        if self.reverse:
+            z, log_det = self.mprqat(z)
+        else:
+            z, log_det = self.mprqat.inverse(z)
+        return z, log_det.view(-1)
+
 
 # Layers for feature/channel mixing
 
