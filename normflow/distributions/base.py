ec553caecb9b0c98e8822ff7ba772bcb488bbeff
Vincent Stimper <vincent.stimper@gmail.com>
2022-03-24 15:13:58 +0100

    Added separate sampling function

--- normflow/distributions/base.py
+++ normflow/distributions/base.py
@@ -118,13 +118,16 @@ class UniformGaussian(BaseDistribution):
             self.register_buffer('scale', scale)
 
     def forward(self, num_samples=1):
+        z = self.sample(num_samples)
+        return z, self.log_prob(z)
+
+    def sample(self, num_samples=1):
         eps_u = torch.rand((num_samples, len(self.ind_)), dtype=self.scale.dtype,
                            device=self.scale.device) - 0.5
         eps_g = torch.randn((num_samples, len(self.ind_)), dtype=self.scale.dtype,
                             device=self.scale.device)
         z = torch.cat((eps_u, eps_g), -1)
-        z = self.scale * z
-        return z, self.log_prob(z)
+        return self.scale * z
 
     def log_prob(self, z):
         log_p_u = torch.broadcast_to(-torch.log(self.scale[self.ind]), (len(z), -1))
