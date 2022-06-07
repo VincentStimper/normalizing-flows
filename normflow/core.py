Fixed sign of alpha divergence

normflow/core.py
@@ -1,8 +1,9 @@
 import torch
 import torch.nn as nn
+import numpy as np
+
 from . import distributions
 from . import utils
-import math
 
 class NormalizingFlow(nn.Module):
     """
@@ -90,8 +91,7 @@ class NormalizingFlow(nn.Module):
             weights = (1 - alpha) * w_alpha + alpha * w_alpha ** 2
             loss = -alpha * torch.mean(weights * torch.log(w))
         else:
-            loss = -torch.logsumexp(alpha * (log_p - log_q), 0) + \
-                math.log(log_p.shape[0])
+            loss = np.sign(alpha - 1) * torch.logsumexp(alpha * (log_p - log_q), 0)
         return loss
 
     def sample(self, num_samples=1):