2fa27a539a2d283f4853f2a164dc82fabd4339f8
Klaus-Rudolf Kladny <kkladny@ethz.ch>
2023-07-06 16:09:41 +0200

    Create base for general conditional flow

--- normflows/core.py
+++ normflows/core.py
@@ -212,6 +212,177 @@ class NormalizingFlow(nn.Module):
         """
         self.load_state_dict(torch.load(path))
 
+class CondFlow(nn.Module):
+    """
+    Normalizing Flow model to approximate target distribution, with context layers
+    """
+
+    def __init__(self, q0, flows, p=None):
+        """Constructor
+
+        Args:
+          q0: Base distribution
+          flows: List of flows
+          p: Target distribution
+        """
+        super().__init__()
+        self.q0 = q0
+        self.flows = nn.ModuleList(flows)
+        self.p = p
+
+    def forward(self, z, context):
+        """Transforms latent variable z to the flow variable x
+
+        Args:
+          z: Batch in the latent space
+          context: Batch in the context space
+
+        Returns:
+          Batch in the space of the target distribution
+        """
+        for flow in self.flows:
+            if flow.__class__.__name__ == 'MaskedAffineAutoregressive':
+                z, _ = flow(z, context)
+            else:
+                z, _ = flow(z)
+        return z
+
+    def forward_and_log_det(self, z, context):
+        """Transforms latent variable z to the flow variable x and
+        computes log determinant of the Jacobian
+
+        Args:
+          z: Batch in the latent space
+
+        Returns:
+          Batch in the space of the target distribution,
+          log determinant of the Jacobian
+        """
+        log_det = torch.zeros(len(z), device=z.device)
+        for flow in self.flows:
+            if flow.__class__.__name__ == 'MaskedAffineAutoregressive':
+                z, log_d = flow(z, context)
+            else:
+                z, log_d = flow(z)
+            log_det += log_d
+        return z, log_det
+
+    def inverse(self, x, context):
+        """Transforms flow variable x to the latent variable z
+
+        Args:
+          x: Batch in the space of the target distribution
+
+        Returns:
+          Batch in the latent space
+        """
+        for i in range(len(self.flows) - 1, -1, -1):
+            if self.flows[i].__class__.__name__ == 'MaskedAffineAutoregressive':
+                x, _ = self.flows[i].inverse(x, context)
+            else:
+                x, _ = self.flows[i].inverse(x)
+        return x
+
+    def inverse_and_log_det(self, x, context):
+        """Transforms flow variable x to the latent variable z and
+        computes log determinant of the Jacobian
+
+        Args:
+          x: Batch in the space of the target distribution
+
+        Returns:
+          Batch in the latent space, log determinant of the
+          Jacobian
+        """
+        log_det = torch.zeros(len(x), device=x.device)
+        for i in range(len(self.flows) - 1, -1, -1):
+            if self.flows[i].__class__.__name__ == 'MaskedAffineAutoregressive':
+                x, log_d = self.flows[i].inverse(x, context)
+            else:
+                x, log_d = self.flows[i].inverse(x)
+            log_det += log_d
+        return x, log_det
+
+    def forward_kld(self, x, context):
+        """Estimates forward KL divergence, see [arXiv 1912.02762](https://arxiv.org/abs/1912.02762)
+
+        Args:
+          x: Batch sampled from target distribution
+          context: Batch sampled from context distribution
+
+        Returns:
+          Estimate of forward KL divergence averaged over batch
+        """
+        log_q = torch.zeros(len(x), device=x.device)
+        z = x
+        for i in range(len(self.flows) - 1, -1, -1):
+            if self.flows[i].__class__.__name__ == 'MaskedAffineAutoregressive':
+                z, log_det = self.flows[i].inverse(z, context)
+            else:
+                z, log_det = self.flows[i].inverse(z)
+            log_q += log_det
+        log_q += self.q0.log_prob(z)
+        return -torch.mean(log_q)
+
+    def reverse_kld(self, num_samples=1, beta=1.0, score_fn=True):
+        raise NotImplementedError
+
+    def reverse_alpha_div(self, num_samples=1, alpha=1, dreg=False):
+        raise NotImplementedError
+
+    def sample(self, context, num_samples=1):
+        """Samples from flow-based approximate distribution
+
+        Args:
+          num_samples: Number of samples to draw
+
+        Returns:
+          Samples, log probability
+        """
+        z, log_q = self.q0(num_samples)
+        for flow in self.flows:
+            if flow.__class__.__name__ == 'MaskedAffineAutoregressive':
+                z, log_det = flow(z, context)
+            else:
+                z, log_det = flow(z)
+            log_q -= log_det
+        return z, log_q
+
+    def log_prob(self, x, context):
+        """Get log probability for batch
+
+        Args:
+          x: Batch
+
+        Returns:
+          log probability
+        """
+        log_q = torch.zeros(len(x), dtype=x.dtype, device=x.device)
+        z = x
+        for i in range(len(self.flows) - 1, -1, -1):
+            if self.flows[i].__class__.__name__ == 'MaskedAffineAutoregressive':
+                z, log_det = self.flows[i].inverse(z, context)
+            else:
+                z, log_det = self.flows[i].inverse(z)
+            log_q += log_det
+        log_q += self.q0.log_prob(z)
+        return log_q
+
+    def save(self, path):
+        """Save state dict of model
+
+        Args:
+          path: Path including filename where to save model
+        """
+        torch.save(self.state_dict(), path)
+
+    def load(self, path):
+        """Load model from state dict
+
+        Args:
+          path: Path including filename where to load model from
+        """
+        self.load_state_dict(torch.load(path))
 
 class ClassCondFlow(nn.Module):
     """
